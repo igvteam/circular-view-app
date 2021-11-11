37dee7d8c9e61045a37302c557a7a9b72f28a937
jrobinso <jrobinso@broadinstitute.org>
2021-11-11 09:37:37 -0800

    .

--- main.js
+++ main.js
@@ -1,43 +1,153 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
+const {app, BrowserWindow, ipcMain, Menu} = require('electron');
+const path = require('path');
+const createMenu = require('./menu.js');
 
-function createWindow () {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 800,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+// Prevent window from being garbage collected
+let mainWindow;
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+const createMainWindow = async () => {
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-}
+    mainWindow = new BrowserWindow({
+        title: "IGV",
+        show: false,
+        width: 600,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, "preload.js")
+        }
+    });
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  createWindow()
+    mainWindow.on('ready-to-show', () => {
+        mainWindow.show();
+    });
 
-  app.on('activate', function () {
+    mainWindow.on('closed', () => {
+        mainWindow = undefined;
+    });
+
+    await mainWindow.loadFile(path.join(__dirname, 'index.html'));
+
+    return mainWindow;
+};
+
+
+(async () => {
+
+    await app.whenReady();
+    mainWindow = await createMainWindow();
+    Menu.setApplicationMenu(createMenu(mainWindow));
+
+})();
+
+app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
+    if (BrowserWindow.getAllWindows().length === 0) createMainWindow()
 })
+
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
+    if (process.platform !== 'darwin') app.quit()
 })
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+
+ipcMain.on('toIGV', (event, msg) => {
+    console.log(msg) // msg from web page
+    sendToIGV(msg)
+})
+
+
+// server (socket) listens for messages from IGV desktop
+
+const net = require('net');
+const port = 1234;
+const host = 'localhost';
+const server = net.createServer();
+
+server.listen(port, host, () => {
+    console.log(`TCP server listening on ${host}:${port}`);
+});
+
+server.on('connection', (socket) => {
+
+    let buffer = [];
+
+    var clientAddress = `${socket.remoteAddress}:${socket.remotePort}`;
+    console.log(`new client connected: ${clientAddress}`);
+
+    socket.on('data', (data) => {
+        console.log(`Client ${clientAddress}: ${data}`);
+        for (let b of data) {
+            if (b === 10) {   // Line feed
+                try {
+                    const message = String.fromCharCode.apply(null, buffer);
+                    const json = JSON.parse(message);
+                    if ('ping' === json.message) {
+
+                    } else {
+                        mainWindow.webContents.send('fromMain', message);
+                    }
+                } catch (e) {
+                    console.error("Malformed message: " + message);
+                }
+                buffer = [];
+            } else {
+                buffer.push(b);
+            }
+        }
+        socket.write('OK\r\n');
+    });
+
+
+// Add a 'close' event handler to this instance of socket
+    socket.on('close', (data) => {
+        console.log(`connection closed: ${clientAddress}`);
+    });
+
+
+// Add a 'error' event handler to this instance of socket
+    socket.on('error', (err) => {
+        console.log(`Error occurred in ${clientAddress}: ${err.message}`);
+    });
+
+});
+
+
+// Socket for sending to igv
+// The port number and hostname of the server.
+function sendToIGV(message) {
+    const igvport = 60151;
+
+// Create a new TCP client.
+    const client = new net.Socket();
+// Send a connection request to the server.
+    client.connect({port: igvport, host: host}, function () {
+        // If there is no error, the server has accepted the request and created a new
+        // socket dedicated to us.
+        console.log('TCP connection established with the server.');
+
+        // The client can now send data to the server by writing to its socket.
+        client.write(message);
+
+        client.end();
+    })
+
+// // The client can also receive data from the server by reading from its socket.
+// client.on('data', function(chunk) {
+//   console.log(`Data received from the server: ${chunk.toString()}.`);
+//
+//   // Request an end to the connection after the data has been received.
+//   client.end();
+// });
+
+    client.on('end', function () {
+        console.log('Requested an end to the TCP connection');
+    });
+}
---
+++ menu.js
@@ -0,0 +1,226 @@
+'use strict';
+const path = require('path');
+const fs = require('fs');
+const {app, Menu, shell, dialog} = require('electron');
+const {
+    is,
+    appMenu,
+    aboutMenuItem,
+    openUrlMenuItem,
+    openNewGitHubIssue,
+    debugInfo
+} = require('electron-util');
+const prompt = require('electron-prompt');
+
+
+let mainWindow;
+
+function createMenu(win) {
+    mainWindow = win;
+    return Menu.buildFromTemplate(template);
+}
+
+const showPreferences = () => {
+    // Show the app's preferences here
+};
+
+const helpSubmenu = [
+    openUrlMenuItem({
+        label: 'Website',
+        url: 'https://igv.org'
+    }),
+    openUrlMenuItem({
+        label: 'Source Code',
+        url: 'https://github.com/igvteam/circular-view.js'
+    }),
+    {
+        label: 'Report an Issue…',
+        click() {
+            const body = `
+<!-- Please succinctly describe your issue and steps to reproduce it. -->
+
+
+---
+
+${debugInfo()}`;
+
+            openNewGitHubIssue({
+                user: 'igvteam',
+                repo: 'circular-view',
+                body
+            });
+        }
+    }
+];
+
+const viewSubmenu = [
+    {
+        label: 'Clear chords',
+        click() {
+            mainWindow.webContents.send('fromMain', '{"message": "clearChords"}');
+        }
+    }
+];
+
+if (!is.macos) {
+    helpSubmenu.push(
+        {
+            type: 'separator'
+        },
+        aboutMenuItem({
+            icon: path.join(__dirname, 'static', 'IGV_64.ico'),
+            text: 'Created by Your Name'
+        })
+    );
+}
+
+const debugSubmenu = [
+    {
+        label: 'Open development tools',
+        click() {
+            // Open the DevTools.
+            mainWindow.webContents.openDevTools()
+        }
+    },
+    // {
+    //     label: 'Show Settings',
+    //     click() {
+    //         config.openInEditor();
+    //     }
+    // },
+    // {
+    //     label: 'Show App Data',
+    //     click() {
+    //         shell.openItem(app.getPath('userData'));
+    //     }
+    // },
+    // {
+    //     type: 'separator'
+    // },
+    // {
+    //     label: 'Delete Settings',
+    //     click() {
+    //         config.clear();
+    //         app.relaunch();
+    //         app.quit();
+    //     }
+    // },
+    // {
+    //     label: 'Delete App Data',
+    //     click() {
+    //         shell.moveItemToTrash(app.getPath('userData'));
+    //         app.relaunch();
+    //         app.quit();
+    //     }
+    // }
+];
+
+const macosTemplate = [
+    appMenu([
+        {
+            label: 'Preferences…',
+            accelerator: 'Command+,',
+            click() {
+                showPreferences();
+            }
+        }
+    ]),
+    // {
+    //     role: 'fileMenu',
+    //     submenu: [
+    //         {
+    //             label: 'Load from file...',
+    //             click: getFileFromUser
+    //         },
+    //         {
+    //             type: 'separator'
+    //         },
+    //         {
+    //             role: 'close'
+    //         }
+    //     ]
+    // },
+    // {
+    //     role: 'editMenu'
+    // },
+    {
+        role: 'viewMenu',
+        submenu: viewSubmenu
+    },
+    {
+        role: 'help',
+        submenu: helpSubmenu
+    },
+    {
+        label: 'Debug',
+        submenu: debugSubmenu
+    }
+];
+
+// Linux and Windows
+const otherTemplate = [
+    {
+        role: 'fileMenu',
+        submenu: [
+            {
+                label: 'Custom'
+            },
+            {
+                type: 'separator'
+            },
+            {
+                label: 'Settings',
+                accelerator: 'Control+,',
+                click() {
+                    showPreferences();
+                }
+            },
+            {
+                type: 'separator'
+            },
+            {
+                role: 'quit'
+            }
+        ]
+    },
+    {
+        role: 'viewMenu',
+        submenu: viewSubmenu
+
+    },
+    {
+        role: 'help',
+        submenu: helpSubmenu
+    }
+];
+
+/**
+ * Open a file dialog to select files.
+ *
+ * @returns {Promise<void>}
+ */
+async function getFileFromUser() {
+
+    const files = await dialog.showOpenDialog(mainWindow, {
+        properties: ['openFile', 'multiSelections']
+    });
+
+    if (!files) {
+        return;
+    }
+
+    const configs = createTrackConfigs(files.filePaths);
+    mainWindow.webContents.send('fromMain', JSON.stringify(configs))
+
+}
+
+const template = is.macos ? macosTemplate : otherTemplate;
+
+if (is.development) {
+    template.push({
+        label: 'Debug',
+        submenu: debugSubmenu
+    });
+}
+
+module.exports = createMenu
